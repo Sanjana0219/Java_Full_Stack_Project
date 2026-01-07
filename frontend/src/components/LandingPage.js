83b511d4ec0489a1d975e847535dff9c91a49294
Sanjana0219 <patkotwarsanjana@gmail.com>
2026-01-07 12:21:16 +0530

    Update LandingPage.js
    update

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -44,7 +44,7 @@ const LandingPage = () => {
               fontSize: isSmallScreen ? '2rem' : 'clamp(2rem, 5vw, 3rem)',
             }}
           >
-            Welcome to Employee Management System
+            Welcome to Dream World
           </Typography>
           <Typography
             variant="h6"
